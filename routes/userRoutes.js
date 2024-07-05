a284c321ef00f555fa6e155ef7351e3fb0765a05
Vijay Krishna <vijaykrishnay07@gmail.com>
2024-07-05 12:43:25 +0530

    Implement Logout Feature by easing token in localStorage

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //POST /api/users
 
@@ -16,18 +18,72 @@ router.post("/users", async (req, res) => {
       });
     }
 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     user = new User({
       username: username,
       email: email,
-      password: password,
+      password: hashedPassword,
     });
 
     await user.save();
-    res.status(201).json(user);
-    console.log(`User credentials are saved in database`);
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) {
+          throw err;
+        }
+        res.json({ token });
+        res.status(201).json(user);
+        console.log(`User credentials are saved in database`);
+      }
+    );
   } catch (error) {
     console.log(error.message);
     res.status(500).send(`Server error! please try again after some time`);
+  }
+});
+
+//Login Route
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: `Invalid Credentials` });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ msg: `You are entered Incorrect Password` });
+    }
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: 3600 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send(`Server Error, please try again later`);
   }
 });
 
